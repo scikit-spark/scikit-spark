20e8481dd9f5eed6c28d34d80c8bbd5027890531
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2018-06-10 19:44:11 +0100

    Fixed two more tests, still 5 broken

--- python/skspark/spark_base_search_cv.py
+++ python/skspark/spark_base_search_cv.py
@@ -100,11 +100,19 @@ class SparkBaseSearchCV(BaseSearchCV):
 
         base_estimator = clone(self.estimator)
 
-        # out = self._run_sklearn_fit(base_estimator, X, y, scorers, fit_params,
-        #                             candidate_params, cv, groups)
-
-        out = self._run_skspark_fit(base_estimator, X, y, scorers, fit_params,
-                                    candidate_params, cv, groups)
+        v = 2
+        if v == 1:
+            out = self._run_sklearn_fit(base_estimator, X, y, scorers,
+                                        fit_params, candidate_params, cv,
+                                        groups)
+        elif v == 2:
+            out = self._run_skspark_fit(base_estimator, X, y, scorers,
+                                        fit_params, candidate_params, cv,
+                                        groups)
+        elif v == 3:
+            out = self._run_skspark_mock_fit(
+                base_estimator, X, y, scorers, fit_params, candidate_params,
+                cv, groups)
 
         # if one choose to see train score, "out" will contain train score info
         if self.return_train_score:
@@ -253,7 +261,6 @@ class SparkBaseSearchCV(BaseSearchCV):
 
         verbose = self.verbose
         error_score = self.error_score
-        fit_params = self.fit_params
         return_train_score = self.return_train_score
 
         def spark_task(tup):
@@ -261,6 +268,58 @@ class SparkBaseSearchCV(BaseSearchCV):
             local_estimator = clone(base_estimator)
             local_X = X_bc.value
             local_y = y_bc.value
+
+            res, error = None, None
+
+            try:
+                res = _fit_and_score(
+                    local_estimator, local_X, local_y, scorers, train, test,
+                    verbose, parameters, fit_params,
+                    return_train_score=return_train_score,
+                    return_parameters=True,
+                    return_n_test_samples=True, return_times=True,
+                    error_score=error_score)
+            except ValueError, e:
+                error = e
+            return index, (res, error)
+
+        indexed_out0 = dict(par_param_grid.map(spark_task).collect())
+
+        for i in range(len(param_grid)):
+            error = indexed_out0[i][-1]
+            if error is not None:
+                raise error
+
+        out = [indexed_out0[idx][0][:-1] for idx in range(len(param_grid))]
+
+        X_bc.unpersist()
+        y_bc.unpersist()
+        return out
+
+    def _run_skspark_mock_fit(self, base_estimator, X, y, scorers, fit_params,
+                              candidate_params, cv, groups):
+        param_grid = [(parameters, train, test) for parameters in
+                      candidate_params
+                      for train, test in list(cv.split(X, y, groups))]
+
+        # Because the original python code expects a certain order for the
+        # elements, we need to respect it.
+        indexed_param_grid = list(zip(range(len(param_grid)), param_grid))
+        # par_param_grid = self.spark.sparkContext.parallelize(
+        #     indexed_param_grid, len(indexed_param_grid))
+
+        # X_bc = self.spark.sparkContext.broadcast(X)
+        # y_bc = self.spark.sparkContext.broadcast(y)
+
+        verbose = self.verbose
+        error_score = self.error_score
+        return_train_score = self.return_train_score
+
+        def spark_task(tup):
+            (index, (parameters, train, test)) = tup
+            local_estimator = clone(base_estimator)
+            local_X = X
+            local_y = y
             res = _fit_and_score(
                 local_estimator, local_X, local_y, scorers, train, test,
                 verbose, parameters, fit_params,
@@ -270,8 +329,17 @@ class SparkBaseSearchCV(BaseSearchCV):
                 error_score=error_score)
             return index, res
 
-        indexed_out0 = dict(par_param_grid.map(spark_task).collect())
-        return [indexed_out0[idx][:-1] for idx in range(len(param_grid))]
+        # indexed_out0 = dict(par_param_grid.map(spark_task).collect())
+
+        indexed_out0 = {}
+        for i, grid in indexed_param_grid:
+            index, res = spark_task((i, grid))
+            indexed_out0[index] = res
+        out = [indexed_out0[idx][:-1] for idx in range(len(param_grid))]
+
+        # X_bc.unpersist()
+        # y_bc.unpersist()
+        return out
 
     def __getstate__(self):
         """To not try to pickle the non-serializable SparkSession"""
