126dddd07530e55de27c2338ba8cd75898d5e9a0
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2018-06-10 08:52:01 +0100

    Added some basic tests for direct comparison to non-spark version

---
+++ python/skspark/tests/test_direct_comparison.py
@@ -0,0 +1,44 @@
+from sklearn import datasets
+from sklearn.model_selection import GridSearchCV as SklearnGridSearchCV
+from sklearn.model_selection import RandomizedSearchCV \
+    as SklearnRandomizedSearchCV
+from sklearn.svm import SVC
+
+from skspark.model_selection import GridSearchCV as SparkGridSearchCV
+from skspark.model_selection import RandomizedSearchCV \
+    as SparkRandomizedSearchCV
+from skspark.tests.pyspark_test import PySparkTest
+
+
+class TestDirectComparison(PySparkTest):
+
+    def test_example_grid_search(self):
+        # The classic example from the sklearn documentation
+        iris = datasets.load_iris()
+        parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
+        svr = SVC()
+        clf = SklearnGridSearchCV(svr, parameters)
+        clf.fit(iris.data, iris.target)
+
+        clf2 = SparkGridSearchCV(self.spark, svr, parameters)
+        clf2.fit(iris.data, iris.target)
+
+        b1 = clf.estimator
+        b2 = clf2.estimator
+        self.assertEqual(b1.get_params(), b2.get_params())
+
+    def test_example_randomized_search(self):
+        # The classic example from the sklearn documentation
+        iris = datasets.load_iris()
+        parameters = {'kernel': ('linear', 'rbf'), 'C': range(1, 10)}
+        svr = SVC()
+        clf = SklearnRandomizedSearchCV(svr, parameters, random_state=4)
+        clf.fit(iris.data, iris.target)
+
+        clf2 = SparkRandomizedSearchCV(self.spark, svr, parameters,
+                                       random_state=4)
+        clf2.fit(iris.data, iris.target)
+
+        b1 = clf.estimator
+        b2 = clf2.estimator
+        self.assertEqual(b1.get_params(), b2.get_params())
