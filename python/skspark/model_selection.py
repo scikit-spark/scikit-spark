999073f3df9f20a1e76423eb88b37ef2de21e19d
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2019-10-13 21:37:08 +0100

    Pulled out 0.20 code required and have separated part that needs to be overwritten as a spark version

--- python/skspark/model_selection.py
+++ python/skspark/model_selection.py
@@ -3,3 +3,5 @@ import sklearn
 
 if sklearn.__version__.startswith("0.19."):
     from skspark.sklearn_0_19.model_selection import GridSearchCV, RandomizedSearchCV
+elif sklearn.__version__.startswith("0.20."):
+    from skspark.sklearn_0_20._search import GridSearchCV, RandomizedSearchCV
