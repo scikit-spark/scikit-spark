f3d77b8d2fe6b520aee88fbbc48d0d3b16ac36cc
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2019-10-18 10:41:35 +0100

    Fixes for 0.19 to work again

--- python/test/resource_warning_tests.py
+++ python/test/resource_warning_tests.py
@@ -8,6 +8,7 @@ This file has this test whilst swallowing the resource warning.
 
 import sys
 import warnings
+from unittest import skipIf
 
 import numpy as np
 from sklearn.datasets import make_blobs
@@ -20,6 +21,7 @@ from sklearn.utils.testing import assert_warns_message
 from sklearn.utils.testing import clean_warning_registry
 
 from skspark.model_selection import GridSearchCV, RandomizedSearchCV
+from test.sklearn_version_specific_utils import sklearn_is_0_19
 
 if sys.version_info[0] > 2:
     from . pyspark_test import PySparkTest
@@ -52,31 +54,27 @@ def assert_no_warnings(func, *args, skip_warning_classes=(ResourceWarning,), **k
 class ResourceWarningTests(PySparkTest):
     """This class contains some tests which break due to returning
     ResourceWarnings. The sklearn versions are skipped in favour of these"""
-    def test_return_train_score_warn(self):
+
+    @skipIf(not sklearn_is_0_19(), "0.19 implementation of test")
+    def test_return_train_score_warn_0_19(self):
         # Test that warnings are raised. Will be removed in 0.21
 
         X = np.arange(100).reshape(10, 10)
         y = np.array([0] * 5 + [1] * 5)
         grid = {'C': [1, 2]}
 
-        estimators = [
-            GridSearchCV(LinearSVC(random_state=0), grid, cv=3, spark=True, iid=True),
-            RandomizedSearchCV(
-                LinearSVC(random_state=0), grid, n_iter=2, cv=3, spark=True, iid=True)
-        ]
+        estimators = [GridSearchCV(LinearSVC(random_state=0), grid),
+                      RandomizedSearchCV(LinearSVC(random_state=0), grid,
+                                         n_iter=2)]
 
         result = {}
         for estimator in estimators:
             for val in [True, False, 'warn']:
                 estimator.set_params(return_train_score=val)
-                result[val] = assert_no_warnings(
-                    estimator.fit, X, y,
-                    skip_warning_classes=(ConvergenceWarning, ResourceWarning))\
-                    .cv_results_
+                result[val] = assert_no_warnings(estimator.fit, X, y).cv_results_
 
         train_keys = ['split0_train_score', 'split1_train_score',
-                      'split2_train_score', 'mean_train_score',
-                      'std_train_score']
+                      'split2_train_score', 'mean_train_score', 'std_train_score']
         for key in train_keys:
             msg = (
                 'You are accessing a training score ({!r}), '
@@ -92,6 +90,45 @@ class ResourceWarningTests(PySparkTest):
             if key not in train_keys:
                 assert_no_warnings(result['warn'].get, key)
 
+    @skipIf(sklearn_is_0_19(), "0.20 version of test")
+    def test_return_train_score_warn(self):
+        # Test that warnings are raised. Will be removed in 0.21
+
+        X = np.arange(100).reshape(10, 10)
+        y = np.array([0] * 5 + [1] * 5)
+        grid = {'C': [1, 2]}
+
+        estimators = [GridSearchCV(LinearSVC(random_state=0), grid,
+                                   iid=False, cv=3),
+                      RandomizedSearchCV(LinearSVC(random_state=0), grid,
+                                         n_iter=2, iid=False, cv=3)]
+
+        result = {}
+        for estimator in estimators:
+            for val in [True, False, 'warn']:
+                estimator.set_params(return_train_score=val)
+                fit_func = ignore_warnings(estimator.fit,
+                                           category=ConvergenceWarning)
+                result[val] = assert_no_warnings(fit_func, X, y).cv_results_
+
+        train_keys = ['split0_train_score', 'split1_train_score',
+                      'split2_train_score', 'mean_train_score', 'std_train_score']
+        for key in train_keys:
+            msg = (
+                'You are accessing a training score ({!r}), '
+                'which will not be available by default '
+                'any more in 0.21. If you need training scores, '
+                'please set return_train_score=True').format(key)
+            train_score = assert_warns_message(FutureWarning, msg,
+                                               result['warn'].get, key)
+            assert np.allclose(train_score, result[True][key])
+            assert key not in result[False]
+
+        for key in result['warn']:
+            if key not in train_keys:
+                assert_no_warnings(result['warn'].get, key)
+
+    @skipIf(sklearn_is_0_19(), "test not applicable to sklearn 0.19")
     def test_deprecated_grid_search_iid(self):
         depr_message = ("The default of the `iid` parameter will change from True "
                         "to False in version 0.22")
---
+++ python/test/sklearn_version_specific_utils.py
@@ -0,0 +1,33 @@
+import sklearn
+
+
+def _is_version(version_check: str) -> bool:
+    if sklearn.__version__.startswith(version_check):
+        return True
+    return False
+
+
+def sklearn_is_0_19() -> bool:
+    return _is_version("0.19.")
+
+
+def sklearn_is_0_20() -> bool:
+    return _is_version("0.20.")
+
+
+def get_refactored_tests_to_skip():
+    """These tests have been edited in order to work with spark.
+    They have been moved into this repo e.g. in resource_warning_tests.py"""
+    if sklearn_is_0_19():
+        return [
+            "test_return_train_score_warn",  # moved to resource_warning_tests.py
+        ]
+    elif sklearn_is_0_20():
+        return [
+            "test_return_train_score_warn",  # moved to resource_warning_tests.py
+            "test_deprecated_grid_search_iid",  # moved to resource_warning_tests.py
+            "test_validate_parameter_grid_input"  # not a function, not a test
+        ]
+    else:
+        raise NotImplementedError(
+            "Unsupported sklearn version {}".format(sklearn.__version__))
