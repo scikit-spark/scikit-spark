d482950e58a5ee43b57d09677a248a792519bd6d
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2019-10-18 20:07:39 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="scikit-spark",
-    version="0.1.0",
+    version="0.2.0",
     author="Ganesh N. Sivalingam",
     author_email="g.n.sivalingam@gmail.com",
     description=description,
