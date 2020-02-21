9b72334fb78291dc914caf353ee8fd10f833c208
Ganesh N. Sivalingam <ganesh@parkopedia.com>
2020-02-21 14:39:52 +0000

    copied same tests to skip from sklearn 0.21 support

--- python/test/sklearn_version_specific_utils.py
+++ python/test/sklearn_version_specific_utils.py
@@ -19,6 +19,10 @@ def sklearn_is_0_21():
     return _is_version("0.21.")
 
 
+def sklearn_is_0_22():
+    return _is_version("0.22.")
+
+
 def get_refactored_tests_to_skip():
     """These tests have been edited in order to work with spark.
     They have been moved into this repo e.g. in resource_warning_tests.py"""
@@ -33,6 +37,12 @@ def get_refactored_tests_to_skip():
             "test_validate_parameter_grid_input"  # a function, not a test
         ]
     elif sklearn_is_0_21():
+        return [
+            "test_refit_callable_out_bound",  # parameterized test, moved to test_parameterised_tests
+            "test_deprecated_grid_search_iid",  # moved to resource_warning_tests.py
+            "test_validate_parameter_grid_input",  # parameterized test, moved to test_parameterised_tests
+        ]
+    elif sklearn_is_0_22():
         return [
             "test_refit_callable_out_bound",  # parameterized test, moved to test_parameterised_tests
             "test_deprecated_grid_search_iid",  # moved to resource_warning_tests.py
