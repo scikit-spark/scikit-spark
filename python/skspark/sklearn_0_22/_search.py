79225dd28a7a92b34438b25a86d04a1eb01e1d25
Ganesh N. Sivalingam <ganesh@parkopedia.com>
2020-02-21 14:46:19 +0000

    remove more code that doesn't need to be duplicated

--- python/skspark/sklearn_0_22/_search.py
+++ python/skspark/sklearn_0_22/_search.py
@@ -1,19 +1,13 @@
 import numbers
 import time
-import warnings
 from abc import abstractmethod
-from collections import defaultdict
-from functools import partial
 
 import numpy as np
 from pyspark.sql import SparkSession
-from scipy.stats import rankdata
 from sklearn.base import is_classifier, clone
 from sklearn.metrics._scorer import _check_multimetric_scoring
 from sklearn.model_selection._search import BaseSearchCV, _check_param_grid, ParameterGrid, ParameterSampler
 from sklearn.model_selection._split import check_cv
-from sklearn.model_selection._validation import _aggregate_score_dicts
-from sklearn.utils.fixes import MaskedArray
 from sklearn.utils.validation import indexable, _check_fit_params
 
 from skspark.sklearn_0_21._search import SparkBaseSearchCV as SparkBaseSearchCV_0_21
@@ -163,93 +157,6 @@ class SparkBaseSearchCV(SparkBaseSearchCV_0_21, BaseSearchCV):
         self.n_splits_ = n_splits
 
         return self
-
-    def _format_results(self, candidate_params, scorers, n_splits, out):
-        n_candidates = len(candidate_params)
-
-        # if one choose to see train score, "out" will contain train score info
-        if self.return_train_score:
-            (train_score_dicts, test_score_dicts, test_sample_counts, fit_time,
-             score_time) = zip(*out)
-        else:
-            (test_score_dicts, test_sample_counts, fit_time,
-             score_time) = zip(*out)
-
-        # test_score_dicts and train_score dicts are lists of dictionaries and
-        # we make them into dict of lists
-        test_scores = _aggregate_score_dicts(test_score_dicts)
-        if self.return_train_score:
-            train_scores = _aggregate_score_dicts(train_score_dicts)
-
-        results = {}
-
-        def _store(key_name, array, weights=None, splits=False, rank=False):
-            """A small helper to store the scores/times to the cv_results_"""
-            # When iterated first by splits, then by parameters
-            # We want `array` to have `n_candidates` rows and `n_splits` cols.
-            array = np.array(array, dtype=np.float64).reshape(n_candidates,
-                                                              n_splits)
-            if splits:
-                for split_i in range(n_splits):
-                    # Uses closure to alter the results
-                    results["split%d_%s"
-                            % (split_i, key_name)] = array[:, split_i]
-
-            array_means = np.average(array, axis=1, weights=weights)
-            results['mean_%s' % key_name] = array_means
-            # Weighted std is not directly available in numpy
-            array_stds = np.sqrt(np.average((array -
-                                             array_means[:, np.newaxis]) ** 2,
-                                            axis=1, weights=weights))
-            results['std_%s' % key_name] = array_stds
-
-            if rank:
-                results["rank_%s" % key_name] = np.asarray(
-                    rankdata(-array_means, method='min'), dtype=np.int32)
-
-        _store('fit_time', fit_time)
-        _store('score_time', score_time)
-        # Use one MaskedArray and mask all the places where the param is not
-        # applicable for that candidate. Use defaultdict as each candidate may
-        # not contain all the params
-        param_results = defaultdict(partial(MaskedArray,
-                                            np.empty(n_candidates,),
-                                            mask=True,
-                                            dtype=object))
-        for cand_i, params in enumerate(candidate_params):
-            for name, value in params.items():
-                # An all masked empty array gets created for the key
-                # `"param_%s" % name` at the first occurrence of `name`.
-                # Setting the value at an index also unmasks that index
-                param_results["param_%s" % name][cand_i] = value
-
-        results.update(param_results)
-        # Store a list of param dicts at the key 'params'
-        results['params'] = candidate_params
-
-        # NOTE test_sample counts (weights) remain the same for all candidates
-        test_sample_counts = np.array(test_sample_counts[:n_splits],
-                                      dtype=np.int)
-
-        if self.iid != 'deprecated':
-            warnings.warn(
-                "The parameter 'iid' is deprecated in 0.22 and will be "
-                "removed in 0.24.", FutureWarning
-            )
-            iid = self.iid
-        else:
-            iid = False
-
-        for scorer_name in scorers.keys():
-            # Computed the (weighted) mean and std for test scores alone
-            _store('test_%s' % scorer_name, test_scores[scorer_name],
-                   splits=True, rank=True,
-                   weights=test_sample_counts if iid else None)
-            if self.return_train_score:
-                _store('train_%s' % scorer_name, train_scores[scorer_name],
-                       splits=True)
-
-        return results
 
 
 class GridSearchCV(SparkBaseSearchCV):
@@ -554,7 +461,7 @@ class GridSearchCV(SparkBaseSearchCV):
                  error_score=np.nan, return_train_score=False,
                  spark=True):
         super().__init__(
-            spark=spark,estimator=estimator, scoring=scoring,
+            spark=spark, estimator=estimator, scoring=scoring,
             n_jobs=n_jobs, iid=iid, refit=refit, cv=cv, verbose=verbose,
             pre_dispatch=pre_dispatch, error_score=error_score,
             return_train_score=return_train_score)
