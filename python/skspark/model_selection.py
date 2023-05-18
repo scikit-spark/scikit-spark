29bdcae1e7957a31338f678f3034ae6022a6b1c0
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2023-05-18 08:20:20 +0100

    Clearer NotImplementedError when importing on a version other than 1.x

--- python/skspark/model_selection.py
+++ python/skspark/model_selection.py
@@ -3,7 +3,7 @@ import sklearn
 if sklearn.__version__.startswith("1."):
     from skspark.sklearn_1._search import GridSearchCV, RandomizedSearchCV
 else:
-    raise NotImplementedError("sklearn versions above 1.0 are not yet supported")
+    raise NotImplementedError("Only sklearn 1.x is supported")
 
 
 __all__ = [
