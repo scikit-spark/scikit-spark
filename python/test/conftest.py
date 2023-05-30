59a2fc3564bc9c575bc77333a1ccd6c5cd9af8e0
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2023-05-30 09:31:30 +0100

    Patch tests from sklearn without needed to reimport the tests in this package

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -1,7 +1,12 @@
 import logging
+from functools import wraps
+from unittest.mock import patch
 
 import pytest
 from pyspark.sql import SparkSession
+
+from skspark.model_selection import RandomizedSearchCV as SkSparkRandomizedSearchCV
+from skspark.model_selection import GridSearchCV as SkSparkGridSearchCV
 
 
 @pytest.fixture(scope="session")
@@ -15,3 +20,33 @@ def spark():
     logger.setLevel(logging.WARN)
 
     return spark_session
+
+
+def monkey_patch_decorator(func):
+    """Decorator that patches sklearn imports with those from this module"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with patch.multiple(
+            "sklearn.model_selection.tests.test_search",
+            RandomizedSearchCV=SkSparkRandomizedSearchCV,
+            GridSearchCV=SkSparkGridSearchCV,
+        ):
+            func(*args, **kwargs)
+
+    return wrapper
+
+
+def pytest_runtest_call(item):
+    """Patch the call that actually runs the tests to apply the monkey patch decorator
+
+    This ensures that when we import the sklearn tests there are all automatically
+    monkey patched to use this packages' RandomSearchCV and GridSearchCV classes.
+    This means we can run all of sklearn's tests directly against the class instances
+    here without having to rewrite them.
+
+    See https://docs.pytest.org/en/6.2.x/reference.html#pytest.hookspec.pytest_runtest_call
+    """
+    # Only automatically decorate the tests that are hijacked from sklearn iteself
+    if item.parent.name == "test_sklearn.py":
+        testfunction = item.obj
+        item.obj = monkey_patch_decorator(testfunction)
